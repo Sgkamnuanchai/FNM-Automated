c6224e54e100ca4e6d5434ef3c3e425e5420a553
Sgkamnuanchai <guidekung11@gmail.com>
2025-06-18 14:33:52 +0200

    devapp52

--- Raspi-streamlit/send_peak_min.py
+++ Raspi-streamlit/send_peak_min.py
@@ -1,7 +1,7 @@
 import serial
 import time
 
-ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1.0)
+ser = serial.Serial('/dev/ttyACM1', 115200, timeout=1.0)
 time.sleep(3)
 ser.reset_input_buffer()
 print("Serial connection success")
